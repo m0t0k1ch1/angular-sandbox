e4546212cbdf799e4bafb972dd83af3e5d894e13
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-04-30 20:39:48 +0900

    feat: add unwallet client sdk page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 
+import { UnWalletClientSDKPageComponent } from './pages/unwallet-client-sdk-page/unwallet-client-sdk-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
+  {
+    path: 'unwallet-client-sdk',
+    component: UnWalletClientSDKPageComponent,
+  },
   {
     path: '**',
     component: NotFoundPageComponent,
---
+++ src/app/pages/unwallet-client-sdk-page/unwallet-client-sdk-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unwallet-client-sdk-page',
+  imports: [],
+  templateUrl: './unwallet-client-sdk-page.component.html',
+  styleUrl: './unwallet-client-sdk-page.component.css',
+})
+export class UnWalletClientSDKPageComponent {}
