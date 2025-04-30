87ad99ee03b2d8f54624213d11cee8c881f2843a
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-04-30 20:01:42 +0900

    feat: introduce primeng and tailwindcss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     provideRouter(routes),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+          darkModeSelector: false,
+        },
+      },
+    }),
   ],
 };
