de4dd99bf7941aebfb6112c4be27c32216198be2
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-04-30 20:31:26 +0900

    feat: implement not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+export const routes: Routes = [
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
