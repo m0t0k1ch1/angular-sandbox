b9b0a7e2efef7971d63a7b8311337f240d0d1c8f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-04-30 21:13:33 +0900

    feat(unwallet-client-sdk): add authorize button

--- src/app/pages/unwallet-client-sdk-page/unwallet-client-sdk-page.component.ts
+++ src/app/pages/unwallet-client-sdk-page/unwallet-client-sdk-page.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { initialized, maybeInitialized } from '@m0t0k1ch1/with-state';
+import { ButtonModule } from 'primeng/button';
+import { UnWallet } from 'unwallet-client-sdk';
+
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-unwallet-client-sdk-page',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './unwallet-client-sdk-page.component.html',
   styleUrl: './unwallet-client-sdk-page.component.css',
 })
-export class UnWalletClientSDKPageComponent {}
+export class UnWalletClientSDKPageComponent implements OnInit {
+  public unWallet = maybeInitialized<UnWallet>();
+
+  public ngOnInit(): void {
+    this.init();
+  }
+
+  private async init(): Promise<void> {
+    this.unWallet = initialized(
+      await UnWallet.init({
+        clientID: environment.unWalletClientSDK.clientID,
+      }),
+    );
+  }
+
+  public onClickAuthorizeButton(): void {
+    if (!this.unWallet.isInitialized) {
+      return;
+    }
+
+    this.unWallet.data.authorize({
+      redirectURL: environment.unWalletClientSDK.redirectURL,
+    });
+  }
+}
