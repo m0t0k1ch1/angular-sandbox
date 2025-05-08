ab4b74b96cf2cd9eae3d6c07817f42bf46ad4d59
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-05-08 18:37:46 +0900

    chore: update port

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,6 @@ import { Environment } from '../app/interfaces';
 export const environment: Environment = {
   unWalletClientSDK: {
     clientID: 'CXqlJpKYiXLzVuq3mU49UY5UFn2N3URVt',
-    redirectURL: 'http://localhost:4200/unwallet-client-sdk',
+    redirectURL: 'http://localhost:4201/unwallet-client-sdk',
   },
 };
